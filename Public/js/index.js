885032d0d90056fbdfe878fb36a6725a5ce43b24
rhieeka <rhiavicera@gmail.com>
2023-03-27 00:24:53 +0800

    updated epo

--- Public/js/index.js
+++ Public/js/index.js
@@ -6,3 +6,17 @@ function myForm() {
 function myPassword() {
   alert("Password Reset Successful!");
 }
+
+
+// loader
+$(document).ready(function () {
+  setTimeout(() => {
+    $(".loader").remove();
+  }, 5000); 
+});
+
+// spinner
+$("a[href='#top']").click(function() {
+  $("html, body").animate({ scrollTop: 0 }, "smooth");
+  return false;
+});
